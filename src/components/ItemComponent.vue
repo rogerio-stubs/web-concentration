43d40cb545285172a2d77fd5719e4e33db4a6bd1
rogerio-stubs <rostubs17@gmail.com>
2023-11-29 21:46:21 -0300

    feat: add component to show my to-do

---
+++ src/components/ItemComponent.vue
@@ -0,0 +1,53 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<template>
+  <div class="item">
+    <div>
+      <label for="category">Category:</label>
+      <span id="category">{{ item.category }}</span>
+    </div>
+    <div>
+      <label for="name">Name:</label>
+      <span id="name">{{ item.name }}</span>
+    </div>
+    <div>
+      <label for="description">Description:</label>
+      <span id="description">{{ item.description }}</span>
+    </div>
+    <div>
+      <label for="created">Created:</label>
+      <span id="created">{{ item.created }}</span>
+    </div>
+    <div>
+      <label for="updated">Updated:</label>
+      <span id="updated">{{ item.updated }}</span>
+    </div>
+    <div>
+      <label for="checked">Checked:</label>
+      <input type="checkbox" id="checked" v-model="item.checked" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    originalItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      item: { ...this.originalItem },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div
+    v-for="(categoryItems, categoryName) in categorizedItems"
+    :key="categoryName"
+    class="column">
+      <h2>{{ categoryName }}</h2>
+      <ItemComponent v-for="item in categoryItems" :key="item.id" :originalItem="item"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ItemComponent from '@/components/ItemComponent.vue';
+
+interface Item {
+  id: string;
+  category: string;
+  name: string;
+  description: string;
+  checked: boolean;
+  createdat: string;
+  updatedat: string;
+}
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    ItemComponent,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 'string',
+          category: 'outro',
+          name: 'string',
+          description: 'string',
+          checked: false,
+          createdat: 'Date',
+          updatedat: 'Date',
+        },
+        {
+          id: 'string',
+          category: 'outro',
+          name: 'string',
+          description: 'string',
+          checked: false,
+          createdat: 'Date',
+          updatedat: 'Date',
+        },
+      ],
+    };
+  },
+  computed: {
+    categorizedItems() {
+      const result: Record<string, Item[]> = {};
+      this.items.forEach((item) => {
+        if (!result[item.category]) {
+          result[item.category] = [];
+        }
+        result[item.category].push(item);
+      });
+      return result;
+    },
   },
 });
 </script>
+
+<style scoped>
+.column {
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 20px;
+}
+.item {
+  margin-bottom: 10px;
+}
+</style>
